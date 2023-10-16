20ba117c936102b42f661e91c6624bc40b55b2aa
verakarlsson <veranickolina@hotmail.com>
2023-10-16 12:15:42 +0200

    up

---
+++ script.js
@@ -0,0 +1,4 @@
+var imgs = document.querySelectorAll('.gal img');
+var dots = document.querySelectorAll('.dot');
+var currentImg = 0; // index of the first image 
+const interval = 3000; // duration(speed) of the slide
