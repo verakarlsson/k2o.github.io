41e83916013fdfc58cd52511480edaa1e03651b9
verakarlsson <veranickolina@hotmail.com>
2023-10-20 12:11:15 +0200

    up dates

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+var imgs = document.querySelectorAll('.gal img');
+var dots = document.querySelectorAll('.dot');
+var currentImg = 0; // index of the first image 
+const interval = 3000; // duration(speed) of the slide
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function changeSlide(n) {
+for (var i = 0; i < imgs.length; i++) { // reset
+imgs[i].style.opacity = 0;
+dots[i].className = dots[i].className.replace(' active', '');
 }
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  
+currentImg = n;
+  
+imgs[currentImg].style.opacity = 1;
+dots[currentImg].className += ' active';
 }
 
